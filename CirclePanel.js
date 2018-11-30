1aef85c9d178ed56cc8caf24b26898f90da1c234
Betty Chen <bettyjxchen@gmail.com>
2018-11-30 15:01:00 -0500

    commit

--- CirclePanel.js
+++ CirclePanel.js
@@ -1,24 +1,53 @@
-/*
-* Assignment 10
-* Betty Chen (betty.chen@tufts.edu)
-* Comp86
-*
-* Circle.js
-* The Circle class acts as a vehicle
-* shaped as a circle. Tick allows the
-* vehicle to drive closer or farther
-* by adjusting the Z position.
-*
-*/
+/* Betty Chen, Daniela Chita, Michael Gold
+ * COMP86
+ * Fall 2018
+ * Final Project
+ */
+
+
+/* CirclePanel
+ * CirclePanel
+ */
 
 class CirclePanel {
+<<<<<<< Updated upstream
+	constructor(rMinRatio, rMaxRatio, thetaLeft, thetaRight, 
+	neighbors, letter) {
+
+		// Sizing
+		this.rMinRatio = rMinRatio;
+		this.rMaxRatio = rMaxRatio;
+		this.thetaLeft = thetaLeft;
+		this.thetaRight = thetaRight;
+
+		// This letter
+		this.letter = letter;
+
+		// Related keys (including itself)
+		this.neighbors = neighbors;
+
+		// Draw statuses
+		this.isClicked = false;
+		this.isHovered = false;
+	}
+
+	// Draw this CirclePanel
+	drawSelf(graphics, canvasWidth) {
+
+	}
+
+	// Draw the related Panels
+	drawNeighborPanels(graphics, canvasWidth) {
+		
+	}
+=======
     constructor () {
-    super()
+        super()
     
+>>>>>>> Stashed changes
 
-    }
+	// Draw the related Keys
+	drawNeighborKeys(graphics, canvasWidth) {
 
-    doSomething () { 
-        
-    }
+	}
 }
