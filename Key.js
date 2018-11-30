84d48f88d27bd4ff890e009471d1f83100dcab6c
Betty Chen <bettyjxchen@gmail.com>
2018-11-30 13:48:10 -0500

    first commit

---
+++ Key.js
@@ -0,0 +1,24 @@
+/*
+* Assignment 10
+* Betty Chen (betty.chen@tufts.edu)
+* Comp86
+*
+* Circle.js
+* The Circle class acts as a vehicle
+* shaped as a circle. Tick allows the
+* vehicle to drive closer or farther
+* by adjusting the Z position.
+*
+*/
+
+class Key {
+    constructor () {
+    super()
+    
+
+    }
+
+    doSomething () { 
+        
+    }
+}
---
+++ model.js
@@ -0,0 +1,27 @@
+/*
+* Assignment 10
+* Betty Chen (betty.chen@tufts.edu)
+* Comp86
+*
+* Circle.js
+* The Circle class acts as a vehicle
+* shaped as a circle. Tick allows the
+* vehicle to drive closer or farther
+* by adjusting the Z position.
+*
+*/
+
+class Model {
+    constructor () {
+        super()
+    
+        //variables
+        this.keys = [];
+        this.circlePanels = [];
+
+    }
+
+    populate () { 
+        
+    }
+}
